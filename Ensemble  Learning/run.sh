cb6850eda143f45091046049dd9831852374e003
N8-O <91090976+N8-O@users.noreply.github.com>
2021-10-29 15:31:42 -0600

    Add files via upload

---
+++ Ensemble  Learning/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Running Practice 2a"
+python3.9 AdaBoost.py
+echo "Running Practice 2b"
+python3.9 run_BaggedTreesB.py
+echo "Running Practice 2c"
+python3.9 BaggedTreeVarianceandBias.py
+echo "Running Practice 2d"
+python3.9 run_RandForest.py
+echo "Running Practice 2e"
+python3.9 RandomTreeVarianceandBias.py
